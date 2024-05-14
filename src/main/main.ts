f0f9f2276715bf1ba5961595d1e59a0960a42903
shiki-01 <0110shiki@gmail.com>
2024-05-15 03:58:33 +0900

    Update main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, ipcMain, Menu } from "electron";
+import { app, BrowserWindow, ipcMain, Menu, autoUpdater, dialog } from "electron";
 import fsFunctionListener from "./functions/fsFunction";
 import shellFunctionListener from "./functions/shellFunction";
 import testFunctionListener from "./functions/testFunction";
@@ -126,3 +126,48 @@ app.on("activate", () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
+const server = 'https://update.electronjs.org'
+const feed = `${server}/shiki-01/MarkIn/${process.platform}-${process.arch}/${app.getVersion()}`
+
+if (app.isPackaged) { // パッケージされている（ローカル実行ではない）
+  autoUpdater.setFeedURL({
+    url: feed,
+  });
+  autoUpdater.checkForUpdates(); // アップデートを確認する
+
+  // アップデートのダウンロードが完了したとき
+  autoUpdater.on("update-downloaded", async () => {
+    const returnValue = await dialog.showMessageBox({
+      message: "アップデートあり",
+      detail: "再起動してインストールできます。",
+      buttons: ["再起動", "後で"],
+    });
+    if (returnValue.response === 0) {
+      autoUpdater.quitAndInstall();  // アプリを終了してインストール
+    }
+  });
+
+  // アップデートがあるとき
+  autoUpdater.on("update-available", () => {
+    dialog.showMessageBox({
+      message: "アップデートがあります",
+      buttons: ["OK"],
+    });
+  });
+
+  // アップデートがないとき
+  autoUpdater.on("update-not-available", () => {
+    dialog.showMessageBox({
+      message: "アップデートはありません",
+      buttons: ["OK"],
+    });
+  });
+
+  // エラーが発生したとき
+  autoUpdater.on("error", () => {
+    dialog.showMessageBox({
+      message: "アップデートエラーが起きました",
+      buttons: ["OK"],
+    });
+  });
+}
