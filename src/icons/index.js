70b4ddbdd09e191b9ff5414146f329477adf94bf
shiki-01 <0110shiki@gmail.com>
2024-05-14 15:49:22 +0900

    🚧 wip

--- src/icons/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Check } from '@mui/icons-material/CheckBox';
-export { default as Minus } from '@mui/icons-material/Remove';
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-</script>
-
-<h1>Page</h1>
-<p>こんにちは</p>
-
-<style>
-</style>
